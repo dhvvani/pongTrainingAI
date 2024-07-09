03e693fd215a5ee3e8a21cf27be9cb330bc02799
pateld32 <159044514+pateld32@users.noreply.github.com>
2024-07-09 16:56:27 -0500

    implemented paddle movement

--- pong.py
+++ pong.py
@@ -22,6 +22,7 @@ PADDLE_HEIGHT = 100
 #classes
 class Paddle:
     PADDLE_COLOR = WHITE
+    PADDLE_VELOCITY = 4
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -31,6 +32,13 @@ class Paddle:
     def drawPaddle(self, window):
         pygame.draw.rect(window, self.PADDLE_COLOR, (self.x, self.y, self.width, self.height) )
 
+    def movePaddle(self, up = True):
+        #use min and max to prevent paddles from moving off the window
+        if up:
+            self.y = max(0, self.y - self.PADDLE_VELOCITY)
+        else:
+            self.y = min(HEIGHT - PADDLE_HEIGHT, self.y + self.PADDLE_VELOCITY )
+
 #functions
 def draw(window, paddles):
     window.fill(BLACK)
@@ -38,7 +46,27 @@ def draw(window, paddles):
     for paddle in paddles:
         paddle.drawPaddle(window)
 
+    #draw a line every 20 pixels to draw a dotted line
+    rectWidth = 10
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if( i % 2 == 0):
+            pygame.draw.rect(window, WHITE, (WIDTH/2 - rectWidth//2, i, rectWidth, HEIGHT//20))
+
+
     pygame.display.update()
+
+def handlePaddleMovement(keys, lPaddle, rPaddle):
+    if keys[pygame.K_w]:
+        lPaddle.movePaddle(up=True)
+    if keys[pygame.K_s]:
+        lPaddle.movePaddle(up = False)
+
+    if keys[pygame.K_UP]:
+        rPaddle.movePaddle(up=True)
+    if keys[pygame.K_DOWN]:
+        rPaddle.movePaddle(up = False)
+
+
 
 def main():
     gameRunning = True
@@ -56,6 +84,9 @@ def main():
             if event.type == pygame.QUIT:
                 gameRunning = False
                 break
+
+        keysUsed = pygame.key.get_pressed()
+        handlePaddleMovement(keysUsed, leftPaddle, rightPaddle)
 
     pygame.quit()
 
