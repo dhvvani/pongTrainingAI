b8d33124fef3b829ffd7beddac32fa91c274b737
Dhvani Patel <167631404+dhvvani@users.noreply.github.com>
2025-01-01 13:39:27 -0600

    make ball movement random so the AI moves

--- pong/pingBall.py
+++ pong/pingBall.py
@@ -1,4 +1,7 @@
 import pygame
+import math
+import random
+
 class PingBall:
     # Ping-pong ball variables
     BALL_RADIUS = 10
@@ -9,17 +12,26 @@ class PingBall:
         self.x = self.origX = x
         self.y = self.origY = y
 
+        angle = self.randAngle(-30, 30, [0])
+        pos = 1 if random.random() < 0.5 else -1
+
+        self.xVel = pos * abs(math.cos(angle) * self.MAX_VELOCITY)
+        self.yVel = math.sin(angle) * self.MAX_VELOCITY
+
         self.radius = self.BALL_RADIUS
 
-        self.xVel = self.MAX_VELOCITY
-        self.yVel = 0
+    def randAngle(self, minAngle, maxAngle, excluded):
+        angle = 0
+        while angle in excluded:
+            angle = math.radians(random.randrange(minAngle, maxAngle))
 
+        return angle
 
     def drawBall(self, window):
         pygame.draw.circle(window, self.BALL_COLOR, (self.x, self.y), self.radius)
 
     def moveBall(self):
-        self.x +=self.xVel
+        self.x += self.xVel
         self.y += self.yVel
 
     def reset(self):
@@ -27,5 +39,6 @@ class PingBall:
         self.y = self.origY
 
         #SHOOTS BALL TO OPPONENT
+        angle = self.randAngle(-30, 30, [0])
         self.xVel *= -1
-        self.yVel = 0
+        self.yVel = math.sin(angle) * self.MAX_VELOCITY
